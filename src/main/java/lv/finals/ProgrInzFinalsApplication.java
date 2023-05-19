8a3a775b537c7da06c67847be2e0b354681a2e37
Paula <paula.cielava1998@gmail.com>
2023-05-19 10:51:28 +0300

    New project

---
+++ src/main/java/lv/finals/ProgrInzFinalsApplication.java
@@ -0,0 +1,13 @@
+package lv.finals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrInzFinalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrInzFinalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/finals/ProgrInzFinalsApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.finals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrInzFinalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
